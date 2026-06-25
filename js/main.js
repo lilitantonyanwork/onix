77d8710cbaf7a7c5d85159bf4185f54a22d9f2f1
lilitantonyan <lilit@helix.am>
2026-06-25 17:27:48 +0400

    add files

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menu = document.querySelector('.menu');
     const btn = document.querySelector('.btn__menu');
+    const platform = document.querySelectorAll('.platform__tab');
 
     btn.addEventListener('click', function (e) {
         e.stopPropagation(); // чтобы клик не ушёл дальше
         menu.classList.toggle('open');
     });
+    platform.forEach((item) =>{
+        item.addEventListener('click', function (e) {
+            e.stopPropagation();
+            platform.forEach((allItems) =>{
+                allItems.classList.remove('active');
+            });
+            item.classList.toggle('active');
+        });
+    })
+
+
+
+
 });
